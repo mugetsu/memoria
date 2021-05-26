48d0ddd6572c5b32e183251ce5de50b932ffb2b0
Randell <cprjk.buzz@gmail.com>
2021-05-26 15:28:40 +0800

    feat: Added single app initial setup

---
+++ memoria/AddViewController.swift
@@ -0,0 +1,15 @@
+//
+//  AddViewController.swift
+//  memoria
+//
+//  Created by Randell Quitain on 26/5/21.
+//
+
+import UIKit
+
+class AddViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ memoria/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  memoria
+//
+//  Created by Randell Quitain on 26/5/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
